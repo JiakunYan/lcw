a3d2eb4e3f13a2e3981bdd24b24bf735608b8be7
Jiakun Yan <jiakunyan1998@gmail.com>
2025-05-08 13:04:52 -0500

    require cxx17 for lcw

--- cmake_modules/AddLCW.cmake
+++ cmake_modules/AddLCW.cmake
@@ -1,5 +1,10 @@
 function(add_lcw_executable name)
   add_executable(${name} ${ARGN})
+  set_target_properties(
+    ${name}
+    PROPERTIES CXX_STANDARD 17
+               CXX_STANDARD_REQUIRED ON
+               CXX_EXTENSIONS OFF)
   target_compile_definitions(${name} PRIVATE _GNU_SOURCE)
   target_link_libraries(${name} PRIVATE lcw)
   set_target_properties(${name} PROPERTIES OUTPUT_NAME "lcw_${name}")
